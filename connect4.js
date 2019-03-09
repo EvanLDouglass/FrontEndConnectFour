62e48c96316cbd8079917f1f12f25f80f866f185
Evan <evandoug90@gmail.com>
2019-03-08 18:14:50 -0800

    Initial commit. Basic look finished

---
+++ connect4.js
@@ -0,0 +1,9 @@
+/* 
+ *  Author: Evan Douglass
+ *  Created: March 07 2019
+ *  A simple tic-tac-toe game built with front-end web technologies.
+ */
+
+// Global instruction variables
+var oneInstr = "Player One: Pick a column to place your blue tile."
+var twoInstr = "Player Two: Pick a column to place your red tile."
